405aefa6bb996dc1b4647a5837720ea3aecf1543
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-05-17 09:54:08 +0600

    single filter button re renders menu items

--- src/App.js
+++ src/App.js
@@ -7,15 +7,25 @@ function App() {
   const [menu, setMenu] = useState(items);
   const [catagories, setCatagories] = useState([]);
 
+  const filterCategory = (category) =>{
+    console.log('clicked on filterCategory with ' + category);
+    const newMenu = items.filter( item =>{
+      return item.category === category 
+    })
+    setMenu(newMenu);
+  }
+
+  // console.log( filterCategory('breakfast') );
+
   return (
     <main>
       <section className="menu section">
         <div className="title">
           <h2>Our Menu</h2>
+        <div className="underline"></div> 
         </div>
-        <div className="underline"></div>
-        <Categories/>
-        <Menu key={new Date().getTime()} menuItems={items} />
+        <Categories filterCategory={filterCategory}/>
+        <Menu key={new Date().getTime().toString()} menuItems={menu} />
       </section>
     </main>
   );
--- src/Menu.js
+++ src/Menu.js
@@ -5,12 +5,12 @@ const Menu = ({menuItems}) => {
   return <div className='section-center'>
     {menuItems.map(item =>{
       const {id,title,category,price,img, desc} = item
-      return(<article className='menu-item'>
-        <img className='photo' alt={title} key={id} src={img}></img>
+      return(<article key={id} className='menu-item'>
+        <img className='photo' alt={title}  src={img}></img>
         <div className='item-info'>
           <header>
             <h4>{title}</h4>
-            <h4 className='price'>{price}</h4>
+            <h4 className='price'>${price}</h4>
           </header>
           <p className='item-text'>{desc}</p>
         </div>
