0db11d05f950a595e1a29dc5a51699d818a477db
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2021-05-17 08:28:53 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+function App() {
+  const [menu, setMenu] = useState(items);
+  const [catagories, setCatagories] = useState([]);
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+        </div>
+        <div className="underline"></div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
